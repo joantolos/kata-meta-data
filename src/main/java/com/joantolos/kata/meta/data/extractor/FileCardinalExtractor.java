e390223b6820f63edea17cbe2ec6b39b922901e5
joantolos <joan.tolos@gmail.com>
2021-07-19 10:56:26 +0200

    Adding mov and jpeg formats

--- src/main/java/com/joantolos/kata/meta/data/extractor/FileCardinalExtractor.java
+++ src/main/java/com/joantolos/kata/meta/data/extractor/FileCardinalExtractor.java
@@ -3,7 +3,7 @@ package com.joantolos.kata.meta.data.extractor;
 public class FileCardinalExtractor {
 
     public int extract(String filePath) {
-        String[] tokens = filePath.split("\\.(?=[^\\.]+$)");
-        return Integer.parseInt(filePath.split(" - ")[1].substring(0, filePath.split(" - ")[1].length() - (tokens[1].length() + 1)));
+        String extension = filePath.split("\\.(?=[^\\.]+$)")[1];
+        return Integer.parseInt(filePath.split(" - ")[1].substring(0, filePath.split(" - ")[1].length() - (extension.length() + 1)));
     }
 }
